44f3e9e512f10dc2af7e10574603f8c302c53899
balthu <balthu737@gmail.con>
2026-01-19 21:33:37 -0300

    reordene las carpetas

--- gastos-bot/database/models.py
+++
@@ -1,8 +0,0 @@
-# database/models.py
-class Expense:
-    def __init__(self, id, amount, category, date, description=None):
-        self.id = id
-        self.amount = amount
-        self.category = category
-        self.date = date
-        self.description = description
